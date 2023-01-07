8ef8de0bd160591a8556d872c98b7542ae48c8cf
asaikali <asaikali@gmail.com>
2023-01-06 22:46:57 -0500

    WIP

--- src/main/java/com/example/DatabaseQuoteService.java
+++ src/main/java/com/example/DatabaseQuoteService.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DatabaseQuoteService implements QuoteService {
